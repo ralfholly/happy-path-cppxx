5276b7f2a89bcc5c83b2325915e9488bf76d5c96
Ralf Holly <ralf.holly@approxion.com>
2019-11-26 12:41:09 +0100

    Better output for make_all.sh

--- make_all.sh
+++ make_all.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+indent_output='sed -r s/^/\t/'
+
 for outer_dir in */; do
-    for dir in $outer_dir/*/; do
+    for dir in $outer_dir*/; do
         if [ -f $dir/Makefile ]; then
             (
             cd $dir
-            make clean
-            make test
+            echo [$dir]
+            make clean | $indent_output
+            make test | $indent_output
             )
         fi
     done
